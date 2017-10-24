5c0af60bb38baf8190212746d9ad2cd44d4e4776
Brady <brady.withers@outlook.com>
2017-10-23 18:07:56 -0600

    working total

--- java.js
+++ java.js
@@ -1,22 +1,41 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+
 var audioElement = document.createElement("audio");
       audioElement.setAttribute("src", "push.wav");
 
+var sum1 = parseInt($("#shownumber1"));
+console.log(sum1);
+
+var sum2 = parseInt($("#shownumber2"));
+console.log(sum2);
+
+var counter = 0
+var total = 0
+var counter2 = 0
+
 
 $(".label1").click(function () {
-    $(".shownumber1").text(function(){return ~~$(this).text() + 5});
-    audioElement.play();
-});
-
+	 audioElement.play();
+counter+=5
+total+=5
+$(".total").text(total);
+$("#shownumber1").text(counter);
+ });
 
 
 
 $(".label2").click(function () {
-    $(".shownumber2").text(function(){return ~~$(this).text() + 25});
+    counter2+=25
+    total+=25
+    $(".total").text(total);
+$("#shownumber2").text(counter2);
     audioElement.play();
 });
+
+console.log(total);
+
 
 
 });
