6fee7514b4ed79a2285c3237e4bac5913eab2ece
Brady <brady.withers@outlook.com>
2017-10-24 21:10:56 -0600

    big changes

--- java.js
+++ java.js
@@ -5,37 +5,37 @@ $( document ).ready(function() {
 var audioElement = document.createElement("audio");
       audioElement.setAttribute("src", "push.wav");
 
-var sum1 = parseInt($("#shownumber1"));
-console.log(sum1);
-
-var sum2 = parseInt($("#shownumber2"));
-console.log(sum2);
-
 var counter = 0
 var total = 0
 var counter2 = 0
 
 
 $(".label1").click(function () {
-	 audioElement.play();
+audioElement.play();
 counter+=5
 total+=5
 $(".total").text(total);
 $("#shownumber1").text(counter);
+localStorage.setItem("nickel", counter);
+localStorage.setItem("total", total);
  });
 
 
 
 $(".label2").click(function () {
-    counter2+=25
-    total+=25
-    $(".total").text(total);
+audioElement.play();
+counter2+=25
+total+=25
+$(".total").text(total);
 $("#shownumber2").text(counter2);
-    audioElement.play();
+localStorage.setItem("quarter", counter2);
+localStorage.setItem("total", total);
 });
 
-console.log(total);
-
+var random = localStorage.getItem("nickel");
+console.log(random);
 
 
 });
+
+
